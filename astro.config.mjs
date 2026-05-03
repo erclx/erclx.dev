b3ab49ca0b567595282d2d39484fc0bed74945f4
Eric Le <me@erclx.dev>
2026-05-03 23:36:56 +0200

    ci(deploy): publish to cloudflare pages on push to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   integrations: [react()],
-  site: process.env.ASTRO_SITE,
+  site: 'https://erclx.dev',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
